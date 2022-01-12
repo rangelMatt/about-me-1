bbd10ceb5259a89dc042cc517cae7fa372e89406
Michael Maker <maker.mdm@gmail.com>
2022-01-11 17:25:58 -0800

    encode all user prompt question framework

--- js/app.js
+++ js/app.js
@@ -7,6 +7,7 @@ let user = prompt('Hello, what is your name?').toUpperCase();
 alert(`WELCOME ${user}!! Please enjoy this special guessing game to get to know a bit about me!`);
 
 //Ask user five 'yes' or 'no' questions about me and let them know if they are correct
+//Question 1:COOKIES
 
 let answerCookies = prompt('Do I love cookies?').toLowerCase();
 
@@ -15,7 +16,60 @@ if (answerCookies === 'yes' || answerCookies === 'y' || answerCookies === 'ye' |
 }
 else if (answerCookies === 'no'|| answerCookies === 'n' || answerCookies === 'na' || answerCookies === 'nope' || answerCookies === 'noo'|| answerCookies === 'heck no!') {
   console.log('that answer is....');
-  console.log('VERY INCORRECT! I love cookies &#128540;');
+  console.log('VERY INCORRECT! I love cookies -10xp');
 } else {
-  console.log('Please stick to the rules, PAL');
+  console.log('Please stick to the rules, PAL -10,000xp');
 }
+
+//Question 2:KICKFLIP
+let answerKickflip = prompt('Can I do a kickflip?').toLowerCase();
+
+if (answerKickflip === 'yes' || answerKickflip === 'y' || answerKickflip === 'ye' || answerKickflip === 'ya'|| answerKickflip === 'yerp') {
+  console.log('YES HE FLIPPIN` CAN +10xp');
+}
+else if (answerKickflip === 'no'|| answerKickflip === 'n' || answerKickflip === 'na' || answerKickflip === 'nope' || answerKickflip === 'noo'|| answerKickflip === 'heck no!') {
+  console.log('that answer is....');
+  console.log('sorry you are quite incorrect -10xp');
+} else {
+  console.log('Please stick to the rules, PAL -10,000xp');
+}
+
+//Question 3: SHREK
+let answerGetShrecked = prompt('Do I love cookies?').toLowerCase();
+
+if (answerGetShrecked === 'yes' || answerGetShrecked === 'y' || answerGetShrecked === 'ye' || answerGetShrecked === 'ya'|| answerGetShrecked === 'yerp') {
+  console.log('Heck yes I do. +10xp');
+}
+else if (answerGetShrecked === 'no'|| answerGetShrecked === 'n' || answerGetShrecked === 'na' || answerGetShrecked === 'nope' || answerGetShrecked === 'noo'|| answerGetShrecked === 'heck no!') {
+  console.log('that answer is....');
+  console.log('VERY INCORRECT! I love cookies  -10xp');
+} else {
+  console.log('Please stick to the rules, PAL -10,000xp');
+}
+
+//Question 4: SEATTLE
+let answerSeattle = prompt('Do I love cookies?').toLowerCase();
+
+if (answerSeattle === 'yes' || answerSeattle === 'y' || answerSeattle === 'ye' || answerSeattle === 'ya'|| answerSeattle === 'yerp') {
+  console.log('Heck yes I do. +10xp');
+}
+else if (answerSeattle === 'no'|| answerSeattle === 'n' || answerSeattle === 'na' || answerSeattle === 'nope' || answerSeattle === 'noo'|| answerSeattle === 'heck no!') {
+  console.log('that answer is....');
+  console.log('VERY INCORRECT! I love cookies  -10xp');
+} else {
+  console.log('Please stick to the rules, PAL -10,000xp');
+}
+
+//Question 5: GOTTFRIED
+let answerGottfried = prompt('Do I love cookies?').toLowerCase();
+
+if (answerGottfried === 'yes' || answerGottfried === 'y' || answerGottfried === 'ye' || answerGottfried === 'ya'|| answerGottfried === 'yerp') {
+  console.log('Heck yes I do. +10xp');
+}
+else if (answerGottfried === 'no'|| answerGottfried === 'n' || answerGottfried === 'na' || answerGottfried === 'nope' || answerGottfried === 'noo'|| answerGottfried === 'heck no!') {
+  console.log('that answer is....');
+  console.log('VERY INCORRECT! I love cookies -10xp');
+} else {
+  console.log('Please stick to the rules, PAL -10,000xp');
+}
+
